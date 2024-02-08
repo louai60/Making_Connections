c315cd090cb20fbd407dd014232c6c34de676261
louai60 <louaysaafi60@gmail.com>
2024-02-08 15:43:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var element1 = document.getElementById("conn1");
+var element2 = document.getElementById("conn2");
+
+function remove() {
+    element1.remove("connec");
+}
+
+function remove2() {
+    element2.remove("connec");
+}
+
+//changing Name 
+
+const text = document.querySelector('.name');
+
+function changeName(){
+    text.innerText = "Louay";
+}
